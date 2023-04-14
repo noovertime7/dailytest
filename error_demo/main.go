b3e18ee7bba2904e0e61c74af7f22e7e2bc3b644
chenteng <cc17854330572@163.com>
2023-04-14 14:03:24 +0800

    feat: 优化error demo

--- error_demo/main.go
+++ error_demo/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
-	"math/rand"
+	"github.com/gin-gonic/gin"
+	"log"
+	"strings"
 	"time"
 )
 
 func main() {
 	// 模拟打印error日志 用于日志错误检测demo
-	for {
-		res := rand.Intn(100)
-		if res > 50 {
-			logrus.Error(res)
-		} else {
-			logrus.Info(res)
+	var (
+		traceID = ""
+		spanId  = ""
+	)
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		log.Println("请求头", ctx.Request.Header)
+		allStr := ctx.Request.Header.Get("Traceparent")
+		allStrList := strings.Split(allStr, "-")
+		if len(allStrList) == 4 {
+			traceID = allStrList[1]
+			spanId = allStrList[2]
 		}
-		time.Sleep(3 * time.Second)
-	}
+		log.Printf(`{"level":"error","traceID":"%s","spanId":"%s","message":"error模拟日志"}`, traceID, spanId)
+		ctx.Writer.WriteString("ok")
+	})
+
+	go func() {
+		for {
+			log.Printf(`{"level":"info","traceID":"%s","spanId":"%s","message":"info模拟日志"}`, traceID, spanId)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	r.Run(":8080")
 }
